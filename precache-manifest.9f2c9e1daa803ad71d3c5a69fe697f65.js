a5949d3c5f2a7570adfafd695a70c0db037d539a
Wilhelm Ackermann <wiac0002@gapps.umu.se>
2020-01-16 12:33:23 +0100

    Updates

---
+++ precache-manifest.9f2c9e1daa803ad71d3c5a69fe697f65.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5791f9ec38cd484756281c0b8b0d7079",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3d01527cf5c0b7d5b60d",
+    "url": "/portfolio/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "25bf2bb728a5409d1cea",
+    "url": "/portfolio/static/css/main.34a3492f.chunk.css"
+  },
+  {
+    "revision": "3d01527cf5c0b7d5b60d",
+    "url": "/portfolio/static/js/2.3b31febc.chunk.js"
+  },
+  {
+    "revision": "8683317dcc23646e1d973e729cb6b30a",
+    "url": "/portfolio/static/js/2.3b31febc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25bf2bb728a5409d1cea",
+    "url": "/portfolio/static/js/main.75467d25.chunk.js"
+  },
+  {
+    "revision": "76d44af2e2b0e8b06bd7",
+    "url": "/portfolio/static/js/runtime-main.88527b73.js"
+  },
+  {
+    "revision": "5ddabff9cf6dfaab3d659aaca3737d66",
+    "url": "/portfolio/static/media/8bitcompProject.5ddabff9.png"
+  },
+  {
+    "revision": "2960decb925704feefb3964a01d7bb73",
+    "url": "/portfolio/static/media/CV_Ackermann.2960decb.pdf"
+  },
+  {
+    "revision": "386034c87b5eff9d7bbb9bfd8e327bbe",
+    "url": "/portfolio/static/media/antiTDProject.386034c8.png"
+  },
+  {
+    "revision": "632cbbd660b6d9ca913ee98e23fd67e9",
+    "url": "/portfolio/static/media/back.632cbbd6.svg"
+  },
+  {
+    "revision": "95e3f80baf5f565a2b1d5cdfa739a9f3",
+    "url": "/portfolio/static/media/base.95e3f80b.pdn"
+  },
+  {
+    "revision": "1df85772474d0c785952a154241873ee",
+    "url": "/portfolio/static/media/c.1df85772.svg"
+  },
+  {
+    "revision": "43e865e5c42c6f698be779fafe5d717f",
+    "url": "/portfolio/static/media/chachinoProject.43e865e5.png"
+  },
+  {
+    "revision": "0439905b41ae671fb33cb6dc3eab3e3a",
+    "url": "/portfolio/static/media/chachinoVideo.0439905b.mp4"
+  },
+  {
+    "revision": "316ef66def3db3cef0348aed9a3530b7",
+    "url": "/portfolio/static/media/csharp.316ef66d.eps"
+  },
+  {
+    "revision": "c11b3d572f2210864c095164b0f76117",
+    "url": "/portfolio/static/media/csharp.c11b3d57.svg"
+  },
+  {
+    "revision": "df9c03200ee0900cea09d14ad763e8e5",
+    "url": "/portfolio/static/media/downArrow.df9c0320.svg"
+  },
+  {
+    "revision": "8f100875832c6cb7942877378ed6ee07",
+    "url": "/portfolio/static/media/fullApp.8f100875.png"
+  },
+  {
+    "revision": "2adbcde2c3395bf45687b281e9aa5b6a",
+    "url": "/portfolio/static/media/group.2adbcde2.jpg"
+  },
+  {
+    "revision": "122f0dec4e0ec0814baf84dadd5f443b",
+    "url": "/portfolio/static/media/hierarchy.122f0dec.png"
+  },
+  {
+    "revision": "4e44dd0c26475ff7b15daa723078d5f3",
+    "url": "/portfolio/static/media/img1.4e44dd0c.png"
+  },
+  {
+    "revision": "a3d5833c92b5834013de11f97b725d2a",
+    "url": "/portfolio/static/media/img2.a3d5833c.png"
+  },
+  {
+    "revision": "8fd0e9ca34681dbf06b6b9aee7fbc3ff",
+    "url": "/portfolio/static/media/img3.8fd0e9ca.png"
+  },
+  {
+    "revision": "5337c26913f74fa5bfaec6d1632d9832",
+    "url": "/portfolio/static/media/iteration1.5337c269.jpg"
+  },
+  {
+    "revision": "416f4ce0798880984b669555fb46a7d2",
+    "url": "/portfolio/static/media/iteration2Box.416f4ce0.jpg"
+  },
+  {
+    "revision": "bbaa835cd9b867b33ace2f0873aecd83",
+    "url": "/portfolio/static/media/jag_1.bbaa835c.jpg"
+  },
+  {
+    "revision": "66517ded2b3a6bef72a9c7fc9a1fc931",
+    "url": "/portfolio/static/media/java.66517ded.svg"
+  },
+  {
+    "revision": "4ec1d1a897c9a84163199caeda749a55",
+    "url": "/portfolio/static/media/javaChat.4ec1d1a8.png"
+  },
+  {
+    "revision": "777e4080a59b8a73f602e430b0560d84",
+    "url": "/portfolio/static/media/kaffe.777e4080.jpg"
+  },
+  {
+    "revision": "0d9247a307aa338b61ffcc0664866fad",
+    "url": "/portfolio/static/media/kalimba.0d9247a3.jpg"
+  },
+  {
+    "revision": "cfcfeee3639502aa525b91564d7b3486",
+    "url": "/portfolio/static/media/music.cfcfeee3.svg"
+  },
+  {
+    "revision": "ccb43d3400fb05951c89ad3ba6acd9d5",
+    "url": "/portfolio/static/media/node.ccb43d34.svg"
+  },
+  {
+    "revision": "15b735bad16ac8917069f1af9060ac71",
+    "url": "/portfolio/static/media/other.15b735ba.svg"
+  },
+  {
+    "revision": "83e19a19e5f181ea61d1200e3469b3a9",
+    "url": "/portfolio/static/media/portfolioProject.83e19a19.png"
+  },
+  {
+    "revision": "e89840d9424a605973f7ee3817733160",
+    "url": "/portfolio/static/media/portfolioProject.e89840d9.jpg"
+  },
+  {
+    "revision": "dea1f90eabd637bd8a08f6a5debe35f4",
+    "url": "/portfolio/static/media/radar.dea1f90e.svg"
+  },
+  {
+    "revision": "2d078ee253808f0197a534119ac2d788",
+    "url": "/portfolio/static/media/radioInfoProject.2d078ee2.png"
+  },
+  {
+    "revision": "ebfb3a104c4ad193bf41e7eb5ccb1e29",
+    "url": "/portfolio/static/media/react.ebfb3a10.svg"
+  },
+  {
+    "revision": "e485d7505388259e96cab3f669e2bbef",
+    "url": "/portfolio/static/media/skiss.e485d750.png"
+  },
+  {
+    "revision": "8ab130feec19eea676254c31cb3586a4",
+    "url": "/portfolio/static/media/song1.8ab130fe.webm"
+  },
+  {
+    "revision": "40b1f4aa78e99a8022a3e0a6fd6cac0c",
+    "url": "/portfolio/static/media/song2.40b1f4aa.webm"
+  },
+  {
+    "revision": "604da76434b1a57c59ffdba290dbdb89",
+    "url": "/portfolio/static/media/song3.604da764.webm"
+  },
+  {
+    "revision": "2f1b3c2e869d91b6f578d8c5f8f3117b",
+    "url": "/portfolio/static/media/treasurehuntProject.2f1b3c2e.png"
+  },
+  {
+    "revision": "52f024992c3341d4db0b0839fa55d441",
+    "url": "/portfolio/static/media/uml.52f02499.png"
+  },
+  {
+    "revision": "2b3dc57db3783e289836f212a164db76",
+    "url": "/portfolio/static/media/unity.2b3dc57d.png"
+  },
+  {
+    "revision": "ac38a40cca0cec687be8a0af166f3bf6",
+    "url": "/portfolio/static/media/videoShowcase.ac38a40c.mp4"
+  },
+  {
+    "revision": "031e5fc1d50f811b26bc42d6f23a30bf",
+    "url": "/portfolio/static/media/viewOfGame.031e5fc1.png"
+  },
+  {
+    "revision": "293a87596730b238955ada9c7bcca2a1",
+    "url": "/portfolio/static/media/werewolf.293a8759.png"
+  }
+]);
