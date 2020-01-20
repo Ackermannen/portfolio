c17de2347615855ea16ce09827c2ce38c61c27ee
Wilhelm Ackermann <wiac0002@gapps.umu.se>
2020-01-20 10:58:22 +0100

    fixed image size

--- src/pages/projects/8BitComp.js
+++ src/pages/projects/8BitComp.js
@@ -6,6 +6,7 @@ import FooterWrapper from '../../components/main_page/FooterWrapper';
 import video1 from '../../video/bitcomp/song1.webm';
 import video2 from '../../video/bitcomp/song2.webm';
 import video3 from '../../video/bitcomp/song3.webm';
+import video4 from '../../video/bitcomp/song4.webm';
 
 class BitComp extends Component {
   state = {};
@@ -51,6 +52,14 @@ class BitComp extends Component {
             <Player playsInline src={video3} />
             <p>
               <i>Video 3: Oscilloscope of Song3</i>
+            </p>
+          </div>
+
+          <p></p>
+          <div className="imageWithDesc">
+            <Player playsInline src={video4} />
+            <p>
+              <i>Video 3: Oscilloscope of Song4</i>
             </p>
           </div>
         </div>
